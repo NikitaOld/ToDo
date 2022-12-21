bc506fa1f5dab3c94f39aa160ba898dbe456e7c8
”Bulychev <bulycevn@gmail.com>
2022-12-21 17:27:39 +0300

    addNewTask(task), insertTaskOnHTML(text)

---
+++ main.js
@@ -0,0 +1,27 @@
+const taskList = [];
+const taskAddTaskForm = document.querySelector('.todo__add-task-from');
+const todo__input = document.querySelector('.todo__input');
+todo__input.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter'){
+        let inputValue = todo__input.value;
+        addNewTask(inputValue);
+        insertTaskOnHTML(inputValue);
+    }
+});
+
+function addNewTask(task) {
+    taskList.push(task);
+}
+
+function deleteTask(task){
+
+}
+
+function insertTaskOnHTML(text){
+    taskAddTaskForm.insertAdjacentHTML('afterend',
+        '<form action="" class="todo__task-form">\n' +
+        '        <input class="todo__input-radio" type="radio">\n' +
+        `        <p class="todo__task-text">${text}</p>\n` +
+        '        <span class="close-button"></span>\n' +
+        '    </form>');
+}
