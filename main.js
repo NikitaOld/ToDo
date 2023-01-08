c074fe63de980fe504f8fbc5fa668a78d4125e88
”Bulychev <bulycevn@gmail.com>
2023-01-08 14:16:32 +0300

    2:

--- main.js
+++ main.js
@@ -1,22 +1,62 @@
-const taskList = [];
-const taskAddTaskForm = document.querySelector('.todo__add-task-from');
-const todo__input = document.querySelector('.todo__input');
+const todo = document.querySelector('#todo'),
+      todoForm = document.querySelector('#todo-form'),
+      todoInput = document.querySelector('#todo-input'),
+      todoTaskItems = document.querySelectorAll('.todo__task-item');
 
-function addTask() {
-    let inputValue = todo__input.value;
-    insertTaskOnHTML(inputValue);
-    taskList.push(inputValue);
+function deleteTask(event){
+    const item = event.target.closest('.todo__task-item');
+    item.remove();
 }
 
-function deleteTask(task){
-    task.parentElement.remove();
+function bindEvents(todoItem) {
+    const deleteBtn = todoItem.querySelector('.close-button');
+    deleteBtn.addEventListener('click', deleteTask);
 }
 
-function insertTaskOnHTML(text){
-    taskAddTaskForm.insertAdjacentHTML('afterend',
-        '<div class="todo__task-form">\n' +
-        '        <input class="todo__input-radio" type="radio">\n' +
-        `        <p class="todo__task-text">${text}</p>\n` +
-        '        <span onclick="deleteTask(this)" class="close-button" >\n' +
-        '    </div>');
+function createElement(tag, props, ...children) { // '...' - остаточные параметры
+    const element = document.createElement(tag);
+
+    Object.keys(props).forEach( key => {
+        element[key] = props[key];
+    })
+
+    if (children.length > 0) {
+        children.forEach(child => {
+            if (typeof child === "string") {
+                child = document.createTextNode(child);
+            }
+
+            element.append(child);
+        });
+    }
+
+    return element;
 }
+
+function createToDoItem(text) {
+    const todoInputRadio = createElement('input', { type: 'radio', className: 'todo__input-radio' }),
+          todoTaskText = createElement('textarea', { className: 'todo__task-text' }, text),
+          closeButton = createElement('button', { className: 'close-button' }),
+          todoTaskItem = createElement('div', { className: 'todo__task-item' }, todoInputRadio, todoTaskText, closeButton);
+
+    bindEvents(todoTaskItem);
+
+    return todoTaskItem;
+}
+
+function addToDoItem(event) {
+    event.preventDefault(); // изменение сценария по умолчанию (форма не отправляется на сервер)
+    if (todoInput.value !== ''){
+        const taskItem = createToDoItem(todoInput.value);
+        todo.append(taskItem);
+    } else alert('Нельзя ввести пустуя строку!')
+}
+
+function main(){
+    todoForm.addEventListener("submit", addToDoItem);
+    todoTaskItems.forEach(item => {
+        bindEvents(item);
+    })
+}
+
+main();
